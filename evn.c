1ac511576aab1b81c527acf19cd585c589887fae
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-01-11 16:12:59 +0530

    Create new file

---
+++ evn.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter any number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf(" %d it is an even number");
+}
+else
+{
+printf(" %d it is an odd number");
+}
+}
