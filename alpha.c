8c315ffd2adb2b6315a5871a16e717449c568944
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-01-11 16:29:04 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+printf("enter the character");
+scanf("%c",&ch);
+if(ch=='a'<ch=='z'||ch=='A'<ch=='Z')
+printf("it is a alphabet");
+else
+printf("it is not alphabet");
+}
