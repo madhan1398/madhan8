1ba36560a0667e7a0590c52958e7cfa76df437ca
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-01-18 16:34:56 +0530

    Create interger.c

---
+++ interger.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+long long m;
+int count = 0;
+printf("Enter a integer: ");
+scanf("%lld", &m);
+while(m != 0)
+{
+++count;
+}
+printf("Number of digits is : %d", count);
+}
