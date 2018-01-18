76a0ce8d8df9664c21fa203bc5ad8a9484214847
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-01-18 16:27:49 +0530

    Create int.c

---
+++ int.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a1[10],a2[10];
+printf("the given element is");
+scanf("%d",&a1);
+printf("the given elements is");
+scanf("%d",&a2);
+sum=add();
+}
+add(int a,int b)
+{
+int c;
+printf("the numbers are");
+scanf("%d",a2);
+c=a+b;
+printf("the sum is %d",c);
+return 0;
+}
