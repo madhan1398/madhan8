693c4f730c1ff86d67233aa30191bb2565475a5e
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-01-23 15:44:18 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{ 
+	int a=11;
+	scanf("%d",&a);
+	if(a%2==1)
+	{
+		printf("it is a prime");
+	}
+	else
+	{
+		printf("it is not a prime");
+	}
+	return 0;
+}
