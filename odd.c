14ac2699ea6c28ff03823a3364dc9808c5295c4d
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-01-23 15:57:26 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i,a,b;
+printf("enter the number is",a);
+scanf("%d",&a);
+printf("enter the number is",b);
+scanf("%d",&b);
+for(i=a;i<=b;i++)
+{
+if(i%2!=0)
+printf("%d",i);
+}
+return 0;
+}
