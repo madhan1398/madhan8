22b2ae5cd70442ae6604466e7f3ec178a23c5f9e
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-01-25 16:02:02 +0530

    Create first5.c

---
+++ first5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int n,m=1,i,j=5;
+printf("\nEnter a range: ");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("\n%d\n",(i*j));
+}
+return 0;
+}
