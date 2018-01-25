9f4c7e8dcb69db55471d248351ec40333bf35b24
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-01-25 16:17:12 +0530

    Create arithmatic.c

---
+++ arithmatic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void)
+{
+int m,n,i,sum=0,d;
+printf("\nEnter the initial value: ");
+scanf("%d",&a);
+printf("\nEnter the end value: ");
+scanf("%d",&b);
+printf("\nEnter the difference: ");
+scanf("%d",&d);
+for(i=m;i<=n;i=i+d)
+{
+sum+=i;
+}
+printf("\n%d\n",sum);
+return 0;
+}
