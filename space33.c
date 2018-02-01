69a7935174f2ba86ed19777c706963928a23e8c0
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-02-01 08:57:24 +0000

    Create space33.c

---
+++ space33.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.>
+void main()
+{
+char m[100];
+int count=0,i;
+printf("enter the string");
+scanf("%s",&m);
+for(i=0;m[i]!='\0';i++)
+{
+ count++;
+ printf("increment of the  count");
+ scanf("%d",&count);
+}
+}
