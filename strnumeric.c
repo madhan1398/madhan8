7a6d48e9fc04197046619efe9ae6ca0fbdd9a6f5
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-02-01 08:01:16 +0000

    Create strnumeric.c

---
+++ strnumeric.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,flag=0,n=0;
+clrscr();
+printf("\n enter a string");
+scanf("%d",s);
+for(i=0;s[i]!=0;i++)
+{
+if(s[i]='0';
+{
+flag=flag+1;
+}
+}
+if(flag==0)
+printf("\n string is not contain num"):
+else
+printf("\n string is contain num");
+return 0;
+}
+
