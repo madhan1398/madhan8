6febe8fc3313ef681b3593dfd0ccb03f1f9636b8
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-02-02 14:17:56 +0530

    Create countnumeric35.c

---
+++ countnumeric35.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int m[10],i,n,j;
+for(i=0;i<10;i++)
+{
+printf("Enter the number %d",i);
+scanf("%d",&a[i]);
+}
+n=m[0];
+for(j=0;j<10;j++)
+{
+if(n<a[j])
+n=a[j];
+}
+printf(" The largest number  %d,n");
+return 0;
+}
