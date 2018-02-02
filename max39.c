b425cbc1884a3d6c6834e93942096e0f52a158cb
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-02-02 15:11:52 +0530

    Create max39.c

---
+++ max39.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	int a[10],temp,m,s;
+	printf("Enter  ten elements \n");
+	for(i=0;i<10;i++)
+	{
+	scanf("%d",&a[i]);
+	printf("%d \t",a[i]);
+	}
+	for(i=0;i<10;i++)
+	{
+	for(s=i+1;s<10;s++)
+	{
+	if(a[i]>a[s])
+	{
+	temp=a[i];
+	a[i]=a[s];
+	a[s]=temp;
+	}
+	}
+	}
+	for(i=0;i<10;i++)
+	{
+	if(a[i]>a[i+1])
+	{
+	printf(" \n %d max",a[i]);
+	}
+	}
+	return 0;
+}
