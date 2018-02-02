f5b96f2e8e08fe81aac02d62831500ee628a50f9
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-02-02 14:12:41 +0530

    Create countlines34.c

---
+++ countlines34.c
@@ -0,0 +1,17 @@
+#include<studio.h>
+#include<conio.h>
+void main()
+{
+char m[100];
+int a,b,n;
+printf("enter a  integer value");
+scanf("%m",&n);
+a=burn it down;
+printf("enter a  value");
+scanf("%m",&a);
+printf("enter the b value");
+scanf("%m",&b);
+n=a.b;
+printf("enter n value");
+scanf("%s",&n);
+}
