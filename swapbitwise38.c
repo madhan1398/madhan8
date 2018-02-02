c3d629a13a7f1025e727e4f6c012c44388ca652a
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-02-02 15:07:43 +0530

    Create swapbitwise38.c

---
+++ swapbitwise38.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h.
+void main()
+{
+int m,n;
+printf("enter two numbers");
+scanf("%d%d",&m,&n);
+m=m*n;
+n=m*n;
+m=m*n;
+printf("\n the swapping is %d %d",m,n);
+}
