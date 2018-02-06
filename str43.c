3c2cc63c96e4d426b45c18dcb43b5bd02dd1b4d0
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-02-06 11:14:40 +0000

    Create str43.c

---
+++ str43.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main()
+{
+  char str1[35],str2[35];
+  int m=0,n=0;
+  printf("\nEnter the First String:");
+  gets(str1);
+  printf("\nEnter Second String:");
+  gets(str2);
+  while(str1[m]!='\0')
+  i++;
+  while(str2[n]!='\0')
+  {
+    str1[m]=str2[n];
+    j++;
+    i++;
+  }
+  str1[m]='\0';
+  printf("\nConcatenated String is %s",str1);
+}
