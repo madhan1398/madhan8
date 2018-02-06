cfaafdc8c342dce64acda1cf8b37176f63493b53
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-02-06 11:20:16 +0000

    Create range44.c

---
+++ range44.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+int m;
+printf("enter the number");
+scanf("%d",&m);
+if(m>0&&n<10)
+printf("yes");
+else
+printf("\m no");
+}
