524626d2aa0604e1402d63e2097e302acf32a83d
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-02-13 16:30:43 +0530

    Create print52.c

---
+++ print52.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x;
+printf("enter the number between 1 to 10");
+scanf("%d",&x);
+switch(x)
+{
+case 1:printf("ONE");
+       break;
+case 2:printf("TWO");
+       break;	 
+case 3:printf("THREE");
+       break;
+case 4:printf("FOUR");
+       break;
+case 5:printf("FIVE");
+       break;
+case 6:printf("SIX");
+       break;
+case 7:printf("SEVEN");
+       break;
+case 8:printf("EIGHT");
+       break;
+case 9:printf("NINE");
+       break;
+case 10:printf("TEN");
+       break;	   
+default:printf("number should be within 1 to 10");	
+}
+}
