278e18e54b55ccc326e8a16fd2162c2386c2d657
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-02-22 16:18:16 +0530

    Create alpha56.c

---
+++ alpha56.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    char f[10];
+    int i;
+    scanf("%f",&f);
+    printf("the string is %f",f);
+    if(isalpha(s)&& isdigit(f))
+    { 
+        printf("\n yes");
+    }
+    else
+    {
+        printf("\n no");
+    }
+}
