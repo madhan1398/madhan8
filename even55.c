48a4d36eb66267ace3cfcf00d36d1097a538010f
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-02-22 16:11:38 +0530

    Create even55.c

---
+++ even55.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,f,pro;
+	scanf("%d",&a);
+	scanf("%d",&f);
+	pro=a*f;
+	if(pro%2==0)
+	{
+		printf("it is Even");
+	}
+	else
+	{
+		printf("it is Not Even");
+	}
+}
