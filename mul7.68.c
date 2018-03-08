def29924b5dde516f3728b2bbef2d0520767a8de
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-03-08 16:10:37 +0530

    Create mul7.68.c

---
+++ mul7.68.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int r;
+    printf("enter number:");
+    scanf("%d",&r);
+    if(r%7==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
