1891aaaee52dbb9c2433582118f64ce55e7c3141
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-03-15 14:35:09 +0530

    Create palindrome71.c

---
+++ palindrome71.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+char str[800];
+printf("\n Enter a string:");
+if(str==strrev(str))
+{
+printf("is a Palindrome");
+}
+else
+{
+printf("is a Not palindrome");
+}
