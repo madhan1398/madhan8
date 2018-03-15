4a1ec852edc81cc08ba48cb5d7c0c77c3dd4f396
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-03-15 14:16:26 +0530

    Create odd69.c

---
+++ odd69.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{ 
+int m,r;
+printf("enter the M and R value");
+scanf("%d%d",&M,&R:);
+if((m-r)%2==0)
+printf("is a odd number");
+else
+printf("is a even number");
+}
