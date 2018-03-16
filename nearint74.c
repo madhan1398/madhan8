1f15aefbb088ad641f9fdb4887ee0d8ca8814805
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-03-16 14:13:13 +0530

    Create nearint74.c

---
+++ nearint74.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+#include <math.h>
+void main()
+{
+       float i;
+       printf("Enter  float digit");
+       scanf("%f",&i);
+       printf("round of  %f is  %f\n", i, round(i));
+       getch();
+}
