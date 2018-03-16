0ab4b6999109013b50ce4b4da20a923c59973386
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-03-16 14:07:37 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+int r,l,m,i,flag=0;
+printf("Enter  number:");
+    scanf("%d",&r);
+    printf("Enter starting limit:");
+    scanf("%d",&l);
+    printf("Enter  ending limit:");
+    scanf("%d",&m);
+    for(i=l;i<=m;i++)
+    {
+        if(n==i)
+        {
+            flag=1;
+        }
+    }
+    if(flag==1)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    getch();
+}
