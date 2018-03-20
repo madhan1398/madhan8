76b2e7e1a96795e5e28b2449bc96b8c99518824d
madhan1398 <31653410+madhan1398@users.noreply.github.com>
2018-03-20 16:09:29 +0530

    Rename fact2.c to fact2set.c

---
+++ fact2set.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int r,i,fact=1;
+scanf("%d",&r);
+if(r==0)
+{
+  printf("8");
+}
+else
+{
+for(i=1;i<=n;i++)
+{
+fact=fact*i;
+}
+printf("%d",fact);
+getch();
+}
+
+}
